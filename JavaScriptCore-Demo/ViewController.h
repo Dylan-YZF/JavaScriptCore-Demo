b7332d1c38e404854c5d755bf7a8776bc7f88e3e
Dylan <Dylan@liangshangdeMacBook-Pro.local>
2017-01-05 14:51:24 +0800

    init

---
+++ JavaScriptCore-Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JavaScriptCore-Demo
+//
+//  Created by Dylan on 2017/1/5.
+//  Copyright © 2017年 Dylan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
